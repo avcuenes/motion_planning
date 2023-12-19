d8974fe04ace8a5b9a5a24f0385dd00abe4e36b5
enes <enes.avcu@titra.com.tr>
2023-12-19 13:38:46 +0300

    change mp_node structer

--- src/rrt_star/src/mp_node.cpp
+++ src/rrt_star/src/mp_node.cpp
@@ -1,50 +1,66 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include <std_msgs/msg/float32_multi_array.hpp>
 #include <iostream>
+#include "mp_node.hpp"
 using std::placeholders::_1;
 
-class MinimalSubscriber : public rclcpp::Node
+MP::MP() :  Node("MotionPlanning") 
 {
-public:
-  MinimalSubscriber()
-  : Node("minimal_subscriber")
-  {
-    subscription_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-      "obstacle_point", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-  }
+    obstaclepoints_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
+      "obstacle_point", 10, std::bind(&MP::obstacle_callback, this, _1));
+  
+    homepoint_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
+      "home_point", 10, std::bind(&MP::homepoint_callback, this, _1));
+    
+    targetpoint_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
+      "target_point", 10, std::bind(&MP::targetpoint_callback, this, _1));
+    
+}
 
-private:
-  void topic_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg) const
-  { 
-    const auto obstacle_point_sub = msg->data;
-     RCLCPP_INFO(get_logger(), "Published: [%.2f, %.2f, %.2f]",
-                obstacle_point_sub[0], obstacle_point_sub[1], obstacle_point_sub[2]);
-  }
-  rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr subscription_;
-};
+
+void MP::obstacle_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg)
+{ 
+  const auto obstacle_point_sub = msg->data;
+
+    
+    RCLCPP_INFO(get_logger(), "obstacle point: [%.2f, %.2f, %.2f]",
+              obstacle_point_sub[0], obstacle_point_sub[1], obstacle_point_sub[2]);
+}
+
+
+void MP::homepoint_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg)
+{ 
+  const auto home_point_sub = msg->data;
+  homepoint.x = home_point_sub[0];
+  homepoint.y = home_point_sub[1];
+  homepoint.z = home_point_sub[2];
+  
+    RCLCPP_INFO(get_logger(), "homepoint: [%.2f, %.2f, %.2f]",
+              homepoint.x, homepoint.y, homepoint.z);
+}
+
+
+void MP::targetpoint_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg)
+{ 
+  const auto target_point_sub = msg->data;
+  targetpoint.x = target_point_sub[0];
+  targetpoint.y = target_point_sub[1];
+  targetpoint.z = target_point_sub[2];
+  
+  RCLCPP_INFO(get_logger(), "target_point: [%.2f, %.2f, %.2f]",
+              targetpoint.x,  targetpoint.y,  targetpoint.z);
+}
+
+
 
 int main(int argc, char * argv[])
 { 
   std::cout << "enes "<< std::endl;
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::spin(std::make_shared<MP>());
   rclcpp::shutdown();
   return 0;
 }
